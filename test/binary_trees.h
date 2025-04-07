b286d0015a654bff9bf6ee4d3cfebcc26e9d2412
romain344 <romainleoulzec@gmail.com>
2025-04-07 11:58:30 +0200

    prototype

---
+++ test/binary_trees.h
@@ -0,0 +1,14 @@
+#ifndef
+#define
+#include <stdio.h>
+
+typedef struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+} binary_tree_t;
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+
+#endif
