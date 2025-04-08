d7afd07664cfd6a48d6adee2e6b13b98ffcc187f
romain344 <romainleoulzec@gmail.com>
2025-04-08 12:25:43 +0200

    prototype

--- test/binary_trees.h
+++ test/binary_trees.h
@@ -1,15 +1,30 @@
-#ifndef
-#define
-#include <stdio.h>
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
 
-typedef struct binary_tree_s
+#include <stddef.h>
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
 {
-	int n;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
-} binary_tree_t;
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
 
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+typedef struct binary_tree_s binary_tree_t;
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+void binary_tree_print(const binary_tree_t *tree);
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
-
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+size_t binary_tree_height(const binary_tree_t *tree);
+size_t binary_tree_depth(const binary_tree_t *tree);
 #endif
