ebc551b87ba2904ad9ce469fa946669b702c68c4
romain344 <romainleoulzec@gmail.com>
2025-04-08 15:19:25 +0200

    prototype

---
+++ test/2-binary_tree_insert_right.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * binary_tree_insert_left - function to insert a node as the left child of another node
+ * @parent: pointer to the parent node * @value: value of the node to add
+ *
+ * Return: pointer to the created node or NULL if it fail or if parent is NULL
+ */
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+        binary_tree_t *new_node;
+
+        if (parent == NULL)
+                return (NULL);
+
+        new_node = malloc(sizeof(binary_tree_t));
+        if (new_node == NULL)
+                return (NULL);
+
+        new_node->n = value;
+        new_node->parent = parent;
+        new_node->left = NULL;
+        new_node->right = NULL;
+        if (parent->right != NULL)
+        {
+                new_node->right = parent->right;
+                parent->right->parent = new_node;
+        }
+
+        parent->right = new_node;
+        return (new_node);
+}
--- test/binary_trees.h
+++ test/binary_trees.h
@@ -28,4 +28,5 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 #endif
